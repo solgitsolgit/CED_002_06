2a89e9f322c0f74411ee3ac4d940dcdc01d0ee00
linkjoker12 <linkjoker12@snu.ac.kr>
2025-10-13 15:14:43 +0900

    lab 7 codes

---
+++ 실습 7/7-2/7-2.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+static int counter = 0;
+SoftwareSerial bt_serial(2,3);
+
+void setup() {
+  bt_serial.begin(9600); // Serial baud rate 9600
+}
+
+void loop() {
+  bt_serial.print("Tick #");
+  bt_serial.print(counter++, DEC); // print counter and increment 1
+  bt_serial.print("\n");
+  delay(1000); // delay 1 sec
+}
+
+// CED 002, group 06, 2020-14247 ShinYee Kang
+// CED 002, group 06, 2023-14669 TaeHyun Lim
+// CED 002, group 06, 2025-17066 JungHwan Kim
---
+++ 실습 7/7-3/7-3.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial bt_serial(2, 3); // open bluetooth serial with port 2, 3
+
+void setup() {
+  bt_serial.begin(9600); // serial baud rate 9600
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if (bt_serial.available() > 0) {
+    byte data = (byte) bt_serial.read();
+    if (data == 'h') {
+      digitalWrite(13, HIGH);
+    }
+    if (data == 'l') {
+      digitalWrite(13, LOW);
+    }
+  }
+}
+
+// CED 002, group 06, 2020-14247 ShinYee Kang
+// CED 002, group 06, 2023-14669 TaeHyun Lim
+// CED 002, group 06, 2025-17066 JungHwan Kim
