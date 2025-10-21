382514a911432355b367ee672de92997303b3d07
linkjoker12 <linkjoker12@snu.ac.kr>
2025-10-22 02:33:41 +0900

    push lab09

---
+++ 실습 9/9-1/9-1.ino
@@ -0,0 +1,75 @@
+#define Forward 1 // 전진
+#define Backward 2 // 후진
+#define Turn_Right 3 // 우회전
+#define Turn_Left 4 // 좌회전
+#define Stop 5 // 정지
+
+int direction = 0; // 차량 운행상태 전역변수 선언
+int speed = 200;
+
+#define ENA 6
+#define EN1 7
+#define EN2 3
+#define EN3 4
+#define EN4 2
+#define ENB 5
+
+void setup() {
+  pinMode(ENA, OUTPUT); // ENA
+  pinMode(EN1, OUTPUT); // EN1
+  pinMode(EN2, OUTPUT); // EN2
+  pinMode(ENB, OUTPUT); // ENB
+  pinMode(EN3, OUTPUT); // EN3
+  pinMode(EN4, OUTPUT); // EN4
+}
+
+void loop() {
+  delay(1000); // 1초 delay
+  direction = direction + 1; // 방향 전환 시 사용되는 변수
+
+  if(direction == Forward) // 전진 주행
+  {
+    digitalWrite(EN1, HIGH);
+    digitalWrite(EN2, LOW);
+    analogWrite(ENA, speed);
+    digitalWrite(EN3, HIGH);
+    digitalWrite(EN4, LOW);
+    analogWrite(ENB, speed);
+  }
+
+  else if(direction == Backward) // 후진 주행
+  {
+    digitalWrite(EN1, LOW);
+    digitalWrite(EN2, HIGH);
+    analogWrite(ENA, speed);
+    digitalWrite(EN3, LOW);
+    digitalWrite(EN4, HIGH);
+    analogWrite(ENB, speed);
+  }
+
+  else if(direction == Turn_Right)
+  {
+    digitalWrite(EN1, HIGH);
+    digitalWrite(EN2, LOW);
+    analogWrite(ENA, speed);
+    digitalWrite(EN3, LOW);
+    digitalWrite(EN4, HIGH);
+    analogWrite(ENB, speed);
+  }
+
+  else if(direction == Turn_Left)
+  {
+    digitalWrite(EN1, LOW);
+    digitalWrite(EN2, HIGH);
+    analogWrite(ENA, speed);
+    digitalWrite(EN3, HIGH);
+    digitalWrite(EN4, LOW);
+    analogWrite(ENB, speed);
+  }
+
+  else if(direction == Stop)
+  {
+    analogWrite(ENA, 0);
+    analogWrite(ENB, 0);
+  }
+}
---
+++ 실습 9/9-4/9-4.ino
@@ -0,0 +1,66 @@
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+
+int speed = 200;
+
+#define ENA 6
+#define EN1 7
+#define EN2 3
+#define EN3 4
+#define EN4 2
+#define ENB 5
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  pinMode(ENA, OUTPUT); // ENA
+  pinMode(EN1, OUTPUT); // EN1
+  pinMode(EN2, OUTPUT); // EN2
+  pinMode(ENB, OUTPUT); // ENB
+  pinMode(EN3, OUTPUT); // EN3
+  pinMode(EN4, OUTPUT); // EN4
+
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds/29/2;
+
+}
+
+void goForward() {
+  digitalWrite(EN1, HIGH);
+  digitalWrite(EN2, LOW);
+  analogWrite(ENA, speed);
+  digitalWrite(EN3, HIGH);
+  digitalWrite(EN4, LOW);
+  analogWrite(ENB, speed);
+}
+
+void goBackward() {
+  digitalWrite(EN1, LOW);
+  digitalWrite(EN2, HIGH);
+  analogWrite(ENA, speed);
+  digitalWrite(EN3, LOW);
+  digitalWrite(EN4, HIGH);
+  analogWrite(ENB, speed);
+}
+
+void loop() {
+  long duration, cm;
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+
+  duration = pulseln(ECHO_PIN, HIGH);
+
+  cm = microsecondsToCentimeters(duration);
+
+  if (cm <= 20) {          // 바로 20 사용
+    goBackward();
+    delay(400);            // 잠깐 후진
+  } else {
+    goForward();
+  }
+}
